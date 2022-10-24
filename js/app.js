800e4681125421e9e592e046fef7c952d1449077
IsraChido <2020030223@upsin.edu.mx>
2022-10-23 22:12:57 -0600

    Pares

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ function promedio() {
     let arrPromedio = array.reduce((a, b) => a + b, 0) / array.length;
     imprimir.innerHTML = arrPromedio;
 }
+
+function pares() {
+    let imprimir = document.getElementById('pares');
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let arrPares = array.filter((num) => num % 2 === 0);
+    imprimir.innerHTML = arrPares;
+}
