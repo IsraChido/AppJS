8a1fa702e6d542357da4b16edfba0629aca8706b
IsraChido <2020030223@upsin.edu.mx>
2022-10-23 22:13:17 -0600

    Descendente

--- js/app.js
+++ js/app.js
@@ -11,3 +11,10 @@ function pares() {
     let arrPares = array.filter((num) => num % 2 === 0);
     imprimir.innerHTML = arrPares;
 }
+
+function descendente() {
+    let imprimir = document.getElementById('descendente');
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let arrayDesc = array.sort((a,b)=>b-a)
+    imprimir.innerHTML = arrayDesc;
+}
